61bfa0b486aba902994a2af6d5b97d86c45aee63
pranjalrai-art <pranjalbhs@gmail.com>
2021-04-25 21:17:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+   <div className="box1">
+     Box 1
+   </div> 
+   <div className="box2">
+     Box 2
+   </div>
+   <div className="box3">
+     Box 3
+   </div>
+   <div className="box4">
+     Box 4
+   </div>
     </div>
   );
 }
